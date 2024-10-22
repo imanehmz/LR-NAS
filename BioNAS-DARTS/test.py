687f02d6052cf49918502eee068ad05058552d56
imane h_e <78013834+imanehmz@users.noreply.github.com>
2024-10-22 22:29:37 +0100

    Update test.py

--- BioNAS-DARTS/test.py
+++ BioNAS-DARTS/test.py
@@ -23,7 +23,7 @@ parser.add_argument('--report_freq', type=float, default=50, help='report freque
 parser.add_argument('--gpu', type=int, default=2, help='gpu device id')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
 parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='/home/ih2347/darts/cnn/experiments/search-EXP-20240807-085522/weights.pt', help='path of pretrained model')
+parser.add_argument('--model_path', type=str, default='weights.pt', help='path of pretrained model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
